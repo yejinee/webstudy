e8fd0ce1d8cc2e004335dce4c6d65946cd6af3b2
yejinee <kimyj9609@gmail.com>
2020-05-18 16:35:35 +0900

    2020/05/18 JSP

---
+++ Jsp study/5.18 Servlet Life Cycle/518lecture/src/com/servlet/testservletclass.java
@@ -0,0 +1,55 @@
+package com.servlet;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/tsc")
+public class testservletclass extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("---- doGet() ----");
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+	
+	
+	@PostConstruct
+	private void funPc() {
+		System.out.println("---- @PostConstruct ----");
+
+	}
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("---- INIT ----");
+	}
+	
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("---Destroy----");
+	}
+	
+	@PreDestroy
+	private void funPd() {
+		System.out.println("---- @PreDestroy ----");
+
+	}
+
+}
+
+
