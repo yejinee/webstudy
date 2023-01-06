29ff1cc0205ce2eac9b5286f26f86d5493ec8a8e
Yejin <kimyj9609@gmail.com>
2023-01-06 12:07:14 +0900

    #9 - STEP3 Thread Pool 적용
    Thread Pool을 적용해 안정적인 서비스 구축

--- Spring_Boot/was-practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ Spring_Boot/was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -9,9 +9,12 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
     private final int port;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
     public CustomWebApplicationServer(int port) {
@@ -28,8 +31,8 @@ public class CustomWebApplicationServer {
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected");
 
-                // Connnect될때마다, Thread 생성
-                new Thread(new ClientRequestHandler(clientSocket)).start();
+                // Thread Pool이용해 Thread 생성
+                executorService.execute(new ClientRequestHandler(clientSocket));
 
             }
         }
